beaac92aba486fd563f191305453fbd9294a958a
pietro <pietrolacampora@gmail.com>
2026-08-06 14:42:31 +0200

    feat: display employee contributions

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from decimal import Decimal
 
 from flask import Flask, render_template, request
 
+from salary_calculator.contributions import (
+    calculate_employee_contributions,
+)
 from salary_calculator.validation import parse_gross_salary
 
 app = Flask(__name__)
@@ -10,6 +13,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def home():
     gross_salary: Decimal | None = None
+    employee_contributions: Decimal | None = None
     error_message: str | None = None
 
     if request.method == "POST":
@@ -17,11 +21,15 @@ def home():
 
         try:
             gross_salary = parse_gross_salary(raw_gross_salary)
+            employee_contributions = calculate_employee_contributions(
+                gross_salary,
+            )
         except ValueError as error:
             error_message = str(error)
 
     return render_template(
         "index.html",
         gross_salary=gross_salary,
+        employee_contributions=employee_contributions,
         error_message=error_message,
     )
